8b8923e987a9a8706c898ab0efd32102817604d0
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-03-24 10:52:17 +0900

    fixed

--- v2/cmds.cmd
+++ v2/cmds.cmd
@@ -29,6 +29,9 @@ kubectl apply -f nginx-deployment.yaml
 read -p "Press [Enter] key to resume." 
 clear
 kubectl apply -f nginx_loadbalancer_service.yaml
+read -p "Press [Enter] key to resume." 
+clear
+kubectl get all
 read -p "Press [Enter] key to resume. See the external Site!!!" 
 clear
 kubectl exec -it pod/web-6694cd7775-x4jhk -- /bin/bash
